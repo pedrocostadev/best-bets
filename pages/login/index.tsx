7c6c4e307bae662f07ebbc057668ea84580ab38a
Pedro Costa <pedro.costa.dev@gmail.com>
2021-01-02 21:06:53 +0000

    Show error message on login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import useAuth from '@/hooks/useAuth';
 import FormField from '@/components/formField/FormField';
 import Form from '@/components/form/Form';
+import Text from '@/components/text/Text';
 
 const Login: React.FC = () => {
   const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
 
   const { signIn } = useAuth();
 
@@ -14,10 +16,12 @@ const Login: React.FC = () => {
     router.push((router?.query?.redirectTo || '/') as string);
 
   const shouldSignIn = async (data) => {
+    setError(false);
     const areCredentialsRight = await signIn(data);
     if (areCredentialsRight) {
       goToPage();
     } else {
+      setError(true);
       console.error('Invalid credentials');
     }
   };
@@ -26,6 +30,9 @@ const Login: React.FC = () => {
     <Form submitLabel="Login" onConfirm={shouldSignIn}>
       <FormField required label="Username" type="text" name="userName" />
       <FormField required label="Password" type="password" name="password" />
+      {error && (
+        <Text bold textAlignCenter variant="body1" text="Invalid credentials" />
+      )}
     </Form>
   );
 };
