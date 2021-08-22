48771e4febc35c0725287a65400164cb7b0bc6da
Pedro Costa <pedro.costa.dev@gmail.com>
2021-08-22 13:12:01 +0100

    Filter past fixtures

--- services/fixtures/parser.ts
+++ services/fixtures/parser.ts
@@ -4,23 +4,26 @@ import { Fixture } from './types';
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 const parseFixtures = ({ fixturesData, league }: any): Fixture[] => {
   const { fixtures } = fixturesData.data.api;
+  const now = new Date();
 
-  return fixtures.map((fixture: any) => ({
-    homeTeam: {
-      teamId: fixture.homeTeam.team_id,
-      teamName: fixture.homeTeam.team_name,
-      logo: fixture.homeTeam.logo,
-    },
-    awayTeam: {
-      teamId: fixture.awayTeam.team_id,
-      teamName: fixture.awayTeam.team_name,
-      logo: fixture.awayTeam.logo,
-    },
-    venue: fixture.venue,
-    eventDate: fixture.event_date,
-    leagueId: league.id,
-    fixtureId: fixture.fixture_id,
-  }));
+  return fixtures
+    .filter(({ event_date }) => now <= new Date(event_date))
+    .map((fixture: any) => ({
+      homeTeam: {
+        teamId: fixture.homeTeam.team_id,
+        teamName: fixture.homeTeam.team_name,
+        logo: fixture.homeTeam.logo,
+      },
+      awayTeam: {
+        teamId: fixture.awayTeam.team_id,
+        teamName: fixture.awayTeam.team_name,
+        logo: fixture.awayTeam.logo,
+      },
+      venue: fixture.venue,
+      eventDate: fixture.event_date,
+      leagueId: league.id,
+      fixtureId: fixture.fixture_id,
+    }));
 };
 
 export default { parseFixtures };
